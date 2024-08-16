449caeccc5c370e4240c703971b1630ae508d25f
Bira Botto <tibira.botto@gmail.com>
2024-08-16 14:54:09 -0400

    fix url api

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,43 +1,40 @@
+import { useState, useEffect, useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { api } from "../../services/api";
+import { ProductProps } from "../home";
+import { BsCartPlus } from "react-icons/bs";
+import { CartContext } from "../../contexts/CartContext";
+import toast from "react-hot-toast";
 
-import { useState, useEffect, useContext } from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
-import { api } from '../../services/api'
-import { ProductProps } from '../home'
-import { BsCartPlus } from 'react-icons/bs'
-import { CartContext } from '../../contexts/CartContext'
-import toast from 'react-hot-toast'
-
-export function ProductDetail(){
+export function ProductDetail() {
   const { id } = useParams();
-  const [product, setProduct] = useState<ProductProps>()
+  const [product, setProduct] = useState<ProductProps>();
   const { addItemCart } = useContext(CartContext);
   const navigate = useNavigate();
 
   useEffect(() => {
-    async function getProduct(){
-      const response = await api.get(`/products/${id}`)
+    async function getProduct() {
+      const response = await api.get(`/api/products/${id}`);
       setProduct(response.data);
     }
 
     getProduct();
-  }, [id])
+  }, [id]);
 
-
-  function handleAddItem(product: ProductProps){
+  function handleAddItem(product: ProductProps) {
     toast.success("Produto adicionado no carrinho.", {
-      style:{
+      style: {
         borderRadius: 10,
         backgroundColor: "#121212",
-        color: "#FFF"
-      }
-    })
-    addItemCart(product)
+        color: "#FFF",
+      },
+    });
+    addItemCart(product);
 
-    navigate("/cart")
+    navigate("/cart");
   }
 
-
-  return(
+  return (
     <div>
       <main className="w-full max-w-7xl px-4 mx-auto my-6">
         {product && (
@@ -55,18 +52,20 @@ export function ProductDetail(){
                 <strong className="text-zinc-700/90 text-xl">
                   {product.price.toLocaleString("pt-BR", {
                     style: "currency",
-                    currency: "BRL"
+                    currency: "BRL",
                   })}
                 </strong>
-                <button className="bg-zinc-900 p-1 rounded ml-3" onClick={ () => handleAddItem(product) }>
+                <button
+                  className="bg-zinc-900 p-1 rounded ml-3"
+                  onClick={() => handleAddItem(product)}
+                >
                   <BsCartPlus size={20} color="#FFF" />
                 </button>
               </div>
-
             </div>
           </section>
         )}
       </main>
     </div>
-  )
+  );
 }
