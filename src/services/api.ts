94e8ff6dd1a78ccfb2000f8649e6416dfa1140c9
Bira Botto <tibira.botto@gmail.com>
2024-08-16 14:19:06 -0400

    URL Vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // json-server --watch db.json
 export const api = axios.create({
-  baseURL: "https://cart-context-bira.vercel.app/",
+  baseURL: "https://bira-json-server.vercel.app/",
 });
